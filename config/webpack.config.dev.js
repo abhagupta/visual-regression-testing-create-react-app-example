82861b2b897f98cc74d6c0a8c180c1b37dcc3db0
Abha Gupta <agupta3@walmart.com>
2020-01-20 13:15:55 -0800

    fix building css

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -162,7 +162,8 @@ module.exports = {
             test: /\.styl$/,
             use: [
               'style-loader',
-              'css-loader?modules&camelCase&localIdentName=[path]__[name]__[local]--[hash:base64:5]'
+              'css-loader?modules&camelCase&localIdentName=[path]__[name]__[local]--[hash:base64:5]',
+              'stylus-loader'
             ]
           },
       
